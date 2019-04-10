dd65bdc740c2f09db1e4366e6d0966973ce22ddb
realyungz <realyungz@gmail.com>
2019-04-09 22:25:12 -0400

    pls work

--- twitchb0t.py
+++ twitchb0t.py
@@ -1,118 +1,121 @@
-import socket
-import schedule
 import time
-from requests import get
-from random import randint
-from links import Links
+import socket
 import platform
+from random import randint
+import schedule
+from requests import get
+from links import Links
 
 
 def get_info():
-    with open(info_path) as f:
-        return f.readlines()[0].rstrip()
+    '''gets oauth token from file'''
+    with open(info_path) as file:
+        return file.readlines()[0].rstrip()
 
 def get_rand_clip():
-    with open(clips_path, "r") as f:
-        lines = f.readlines()
+    '''gets a random line from clips.txt'''
+    with open(clips_path, "r") as file:
+        lines = file.readlines()
         return lines[randint(0, len(lines) - 1)]
 
 def request(url):
-    r = get(url).json()
-    return r
+    '''helper function for get request, returns json'''
+    req = get(url).json()
+    return req
 
-def get_epic_id(username):
-    r = request("https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=" + username)
-    if 'uid' in r:
-        return r['uid']
-    else:
-        return None
+def get_epic_id(user):
+    '''function takes an epic username, the accounts user id is returned'''
+    req = request("https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=" + user)
+    if 'uid' in req:
+        return req['uid']
+    return None
 
-def get_fort_stats(id):
-    r = request("https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=" + id)
-    print(r)
-    if 'overallData' not in r:
+def get_fort_stats(epic_id):
+    req = request("https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=" + epic_id)
+    print(req)
+    if 'overallData' not in req:
         return None
-    if 'defaultModes' in r['overallData']:
-        stats   = r['overallData']['defaultModes']
-        if type(stats) is list:
+    if 'defaultModes' in req['overallData']:
+        stats = req['overallData']['defaultModes']
+        if isinstance(stats, list):
             return None
         items = {'kills':0, 'placetop1':0, 'matchesplayed':0}
         for item in items:
             if item in stats:
                 items[item] = stats[item]
-        kd = round(float(items['kills']) / float(items['matchesplayed'] - items['placetop1']), 2)
+        kdr = round(float(items['kills']) / float(items['matchesplayed'] - items['placetop1']), 2)
         win_ratio = round(float(items['placetop1'] / items['matchesplayed']) * 100, 2)
-        return [items['placetop1'], kd, win_ratio]
-    else:
-        return None
+        return [items['placetop1'], kdr, win_ratio]
+    return None
 
-def send_message(message):
-    s.send(bytes("PRIVMSG #" + NICK + " :" + message + "\r\n", "UTF-8"))
+def send_message(msg):
+    '''sends a message to twitch chat'''
+    s.send(bytes("PRIVMSG #" + NICK + " :" + msg + "\r\n", "UTF-8"))
 
-def stats_command(message):
-    username = message.replace("!stats", "")
-    if username.startswith(" ") and len(username) > 4:
-        username = username[1:]
-        id = get_epic_id(username)
-        if id:
-            stats = get_fort_stats(id)
+def stats_command(msg):
+    user = msg.replace("!stats", "")
+    if user.startswith(" ") and len(user) > 4:
+        user = user[1:]
+        epic_id = get_epic_id(user)
+        if epic_id:
+            stats = get_fort_stats(epic_id)
             if stats:
                 send_message("WINS: " + str(stats[0]) + ", K/D: " + str(stats[1]) + ", WIN %: " + str(stats[2]))
             else:
                 send_message("ERROR - user found, but couldn't get stats for some reason ResidentSleeper")
         else:
-            send_message("ERROR - cannot find username: '" + username + "'")
+            send_message("ERROR - cannot find username: '" + user + "'")
     else:
         send_message("ERROR - incorrect format: !stats <epic-username>")
 
 def randclip_command():
     send_message("https://clips.twitch.tv/" + get_rand_clip())
 
-def followage_command(message, username):
-    name = message.replace("!followage", "")
+def followage_command(msg, user):
+    name = msg.replace("!followage", "")
     if name.startswith(" ") and len(name) > 4:
         name = name[1:]
     else:
-        name = username
+        name = user
     followage = get("https://api.crunchprank.net/twitch/followage/RealYungZ/" + name + "?precision=3").text
     send_message(followage)
 
 def retweet_command():
-    rt = get("https://api.crunchprank.net/twitter/latest/RealYungZ?no_rts&url").text
-    send_message(rt)
+    req = get("https://api.crunchprank.net/twitter/latest/RealYungZ?no_rts&url").text
+    send_message(req)
 
 def uptime_command():
     new_time = time.time()
     uptime = time.strftime("%H hours %M minutes", time.gmtime(new_time - start_time))
     send_message("RealYungZ has been live for " + uptime)
 
-def lovemeter_command(message, username):
-    name = message.replace("!lovemeter", "")
+def lovemeter_command(msg, name):
+    name = msg.replace("!lovemeter", "")
     if name.startswith(" ") and len(name) > 2:
         name = name[1:]
-        send_message(username + " is " + str(randint(0,100)) + "% in love with " + name)
+        send_message(name + " is " + str(randint(0, 100)) + "% in love with " + name)
     else:
         send_message("ERROR - incorrect format: !lovemeter <anything>")
 
-def check_mod(username):
-    r = get("http://tmi.twitch.tv/group/user/realyungz/chatters").json()
-    if username in r['chatters']['moderators']:
+def check_mod(name):
+    req = get("http://tmi.twitch.tv/group/user/realyungz/chatters").json()
+    if name in req['chatters']['moderators']:
         return True
     return False
 
-def handle_viewer_command(username, message):
+def handle_viewer_command(name, message):
     if message.startswith("!stats"):
         stats_command(message)
     elif message.startswith("!randclip"):
         randclip_command()
     elif message.startswith("!followage"):
-        followage_command(message, username)
+        followage_command(message, name)
     elif message.startswith("!retweet"):
         retweet_command()
     elif message.startswith("!uptime"):
         uptime_command()
     elif message.startswith("!lovemeter"):
-        lovemeter_command(message, username)
+        lovemeter_command(message, name)
     elif message.startswith("!sub"):
         send_message(Links.SUB)
     elif message.startswith("!tip"):
@@ -134,10 +137,12 @@ def handle_viewer_command(username, message):
     elif message.startswith("!res"):
         send_message("1600x1080p")
     elif message.startswith("!hugme"):
-        send_message("/me hugs "+ username)
+        send_message("/me hugs "+ name)
+    elif message.startswith("!code"):
+        send_message("Code for my twitch bot is here realyuSly " + Links.CODE)
 
-def handle_mod_command(username, message):
-    if check_mod(username):
+def handle_mod_command(name, message):
+    if check_mod(name):
         if message.startswith("&amimod"):
             send_message("you are a mod realyuSwag")
     else:
@@ -152,7 +157,7 @@ def discord_timer():
 info_path = ""
 clips_path = ""
 
-if platform is "Windows":
+if platform.system() is "Windows":
     info_path = "C:\\Users\\Angie\\Desktop\\info.txt"
     clips_path = "C:\\Users\\Angie\\Dev\\twitchb0t\\clips.txt"
 else:
@@ -176,11 +181,10 @@ s.send(bytes("JOIN #" + NICK + "\r\n", "UTF-8"))
 
 
 while True:
-    line = str(s.recv(1024))
-    if "End of /NAMES list" in line:
+    LINE = str(s.recv(1024))
+    if "End of /NAMES list" in LINE:
         print("authenticated")
         break
-
 
 while True:
     for line in str(s.recv(1024)).split('\\r\\n'):
